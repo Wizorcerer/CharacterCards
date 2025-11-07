790e78923e837e1303fc5ff2d48bffa6f6ab727f
Wizorcerer <Elijah.hammer@outlook.com>
2025-11-07 15:54:19 -0700

    c

--- script.js
+++ script.js
@@ -42,68 +42,69 @@ const num = input.value;
 const popUp = document.querySelector('.input');
 const finalize = document.querySelector('#finalize');
 
-    characters.forEach(character => {
-        let html = `<div class="card">
-            <img class="image" src=${character.image}>
-            <h2 class="name">${character.name}</h2>
-            <div class="info">
-                <p><strong>Race: </strong><span id="race">${character.race}</span><p>
-                <p id="class"><strong>Class: Lv</strong><span id="level">${character.level}</span><span>${character.class}</span><p>
-                <p id="alignment"><strong>Alignment: </strong><span id="alignment"">${character.alignment}</span><p>
-                <p id="maxHealth"><strong>Max Health: </strong><span id="max">${character.maxHealth}</span><p>
-            </div>
-            <div class="stats">
-                <p><strong>Str: </strong><span id="strength"">${character.strength}</span><p>
-                <p id="dexterity"><strong>Dex: </strong><span>${character.dexterity}</span><p>
-                <p id="constitution"><strong>Con: </strong><span>${character.constitution}</span><p>
-                <p id="intelligence"><strong>Int: </strong><span>${character.intelligence}</span><p>
-                <p id="wisdom"><strong>Wis: </strong><span>${character.wisdom}</span><p>
-                <p id="charisma"><strong>Cha: </strong><span>${character.charisma}</span><p>
-            </div>
-            <button id="healing" class="buttons">Health</button>
-            <button id="fhealing" class="buttons">Heal (Full)</button>
+characters.forEach(character => {
+    let html = 
+    `<div class="card">
+        <img class="image" src=${character.image}>
+        <h2 class="name">${character.name}</h2>
+        <div class="info">
+            <p><strong>Race: </strong><span id="race">${character.race}</span><p>
+            <p id="class"><strong>Class: Lv</strong><span id="level">${character.level}</span><span>${character.class}</span><p>
+            <p id="alignment"><strong>Alignment: </strong><span id="alignment"">${character.alignment}</span><p>
+            <p id="maxHealth"><strong>Max Health: </strong><span id="max">${character.maxHealth}</span><p>
         </div>
-        <div class="secondary">
-            <div id="top">
-                <img id="shield" src="images/Shield-removebg-preview.png">
-                <img id="wing" src="images/wings-removebg-preview.png">
-                <img id="heart" src="images/heart-removebg-preview.png">
-                <p id="armorClass" class="smstat"><strong>AC</strong><span id="ac">${character.ac}</span><p>
-                <p id="initiative" class="smstat"><strong>Initiative</strong><span id="initiate">${character.initiative}</span><p>
-                <p id="hp" class="smstat"><strong>Health</strong><span id="health">${character.health}</span><p>
-            </div>
-            <div id="other">
-                <h2>Equipment</h2>
-                <p></p>
-                <h2>Proficiencies, and Languages</h2>
-                <p></p>
-                <h2>Feats</h2>
-                <p></p>
-            </div>
-            <div id="skills">
-                <p><strong>Acrobatics: </strong><span id="acrobatics">${character.acrobatics}</span></p>
-                <p><strong>Animal Handling: </strong><span id="animal">${character.animal}</span></p>
-                <p><strong>Arcana: </strong><span id="arcana">${character.arcana}</span></p>
-                <p><strong>Athletics: </strong><span id="athletics">${character.athletics}</span></p>
-                <p><strong>Deception: </strong><span id="deception">${character.deception}</span></p>
-                <p><strong>History: </strong><span id="history">${character.history}</span></p>
-                <p><strong>Insight: </strong><span id="insight">${character.insight}</span></p>
-                <p><strong>Intimidation: </strong><span id="intimidation">${character.intimidation}</span></p>
-                <p><strong>Investigation: </strong><span id="investigation">${character.investigation}</span></p>
-                <p><strong>Medicine: </strong><span id="medicine">${character.medicine}</span></p>
-                <p><strong>Nature: </strong><span id="nature">${character.nature}</span></p>
-                <p><strong>Perception: </strong><span id="perception">${character.perception}</span></p>
-                <p><strong>Performance: </strong><span id="performance">${character.performance}</span></p>
-                <p><strong>Persuasion: </strong><span id="persuasion">${character.persuasion}</span></p>
-                <p><strong>Religion: </strong><span id="religion">${character.religion}</span></p>
-                <p><strong>Sleight of Hand: </strong id="sleight"><span>${character.sleight}</span></p>
-                <p><strong>Stealth: </strong><span id="stealth">${character.stealth}</span></p>
-                <p><strong>Survival: </strong><span id="survival">${character.survival}</span></p>
-            </div>
+        <div class="stats">
+            <p><strong>Str: </strong><span id="strength"">${character.strength}</span><p>
+            <p id="dexterity"><strong>Dex: </strong><span>${character.dexterity}</span><p>
+            <p id="constitution"><strong>Con: </strong><span>${character.constitution}</span><p>
+            <p id="intelligence"><strong>Int: </strong><span>${character.intelligence}</span><p>
+            <p id="wisdom"><strong>Wis: </strong><span>${character.wisdom}</span><p>
+            <p id="charisma"><strong>Cha: </strong><span>${character.charisma}</span><p>
         </div>
-        `;
-        characterList.innerHTML += html;
-    });
+        <button id="healing" class="buttons">Health</button>
+        <button id="fhealing" class="buttons">Heal (Full)</button>
+    </div>
+    <div class="secondary">
+        <div id="top">
+            <img id="shield" src="images/Shield-removebg-preview.png">
+            <img id="wing" src="images/wings-removebg-preview.png">
+            <img id="heart" src="images/heart-removebg-preview.png">
+            <p id="armorClass" class="smstat"><strong>AC</strong><span id="ac">${character.ac}</span><p>
+            <p id="initiative" class="smstat"><strong>Initiative</strong><span id="initiate">${character.initiative}</span><p>
+            <p id="hp" class="smstat"><strong>Health</strong><span id="health">${character.health}</span><p>
+        </div>
+        <div id="other">
+            <h2>Equipment</h2>
+            <p></p>
+            <h2>Proficiencies, and Languages</h2>
+            <p></p>
+            <h2>Feats</h2>
+            <p></p>
+        </div>
+        <div id="skills">
+            <p><strong>Acrobatics: </strong><span id="acrobatics">${character.acrobatics}</span></p>
+            <p><strong>Animal Handling: </strong><span id="animal">${character.animal}</span></p>
+            <p><strong>Arcana: </strong><span id="arcana">${character.arcana}</span></p>
+            <p><strong>Athletics: </strong><span id="athletics">${character.athletics}</span></p>
+            <p><strong>Deception: </strong><span id="deception">${character.deception}</span></p>
+            <p><strong>History: </strong><span id="history">${character.history}</span></p>
+            <p><strong>Insight: </strong><span id="insight">${character.insight}</span></p>
+            <p><strong>Intimidation: </strong><span id="intimidation">${character.intimidation}</span></p>
+            <p><strong>Investigation: </strong><span id="investigation">${character.investigation}</span></p>
+            <p><strong>Medicine: </strong><span id="medicine">${character.medicine}</span></p>
+            <p><strong>Nature: </strong><span id="nature">${character.nature}</span></p>
+            <p><strong>Perception: </strong><span id="perception">${character.perception}</span></p>
+            <p><strong>Performance: </strong><span id="performance">${character.performance}</span></p>
+            <p><strong>Persuasion: </strong><span id="persuasion">${character.persuasion}</span></p>
+            <p><strong>Religion: </strong><span id="religion">${character.religion}</span></p>
+            <p><strong>Sleight of Hand: </strong id="sleight"><span>${character.sleight}</span></p>
+            <p><strong>Stealth: </strong><span id="stealth">${character.stealth}</span></p>
+            <p><strong>Survival: </strong><span id="survival">${character.survival}</span></p>
+        </div>
+    </div>
+    `;
+    characterList.innerHTML += html;
+});
 
 function geraldHeal() {
     popUp.classList.remove('input');
@@ -124,14 +125,6 @@ function yorrimHeal() {
     yorrim.health = yorrim.maxHealth;
     document.querySelector('#health').textContent = yorrim.health;
     document.querySelector('#max').textContent = yorrim.maxHealth
-}
-function yorrimDamaged() {
-    yorrim.health -= 3;
-    console.log(yorrim.health);
-    document.querySelector('#health').textContent = yorrim.health;
-    if (yorrim.health <= 0){
-    alert('Yorrim is dead!');
-    };
 }
 function yorrimFHeal() {
     yorrim.health = yorrim.maxHealth;
